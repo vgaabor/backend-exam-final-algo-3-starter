04b83b3dd757a53bcf9cae4c13277fcf388fa55a
Vajda Gábor <vajda.gaabor@freemail.hu>
2021-05-17 13:44:48 +0200

    Megoldás.

--- src/main/java/com/codecool/Challenge.java
+++ src/main/java/com/codecool/Challenge.java
@@ -3,6 +3,14 @@ package com.codecool;
 public class Challenge {
 
     public static int minPositive(int a, int b) {
-
+        if (a <= 0 && b <= 0) {
+            return -1;
+        } else if (a <= 0) {
+            return b;
+        } else if (b <= 0) {
+            return a;
+        } else {
+            return Math.min(a, b);
+        }
     }
 }
